7872e9d366ae35cf457d3952b5ab8581d4bb733f
hrom <roman@alltmb.ru>
2016-03-05 14:22:30 +0300

    fix Request spec

--- spec/direct/api/v5/request_spec.rb
+++ spec/direct/api/v5/request_spec.rb
@@ -2,15 +2,14 @@ require 'spec_helper'
 
 describe Direct::API::V5::Request do
   let(:client) { make_direct_api_client }
-  let(:request) { described_class.new(client.settings) }
 
   describe '#send' do
-    let(:service) { :Campaigns }
+    let(:service) { :campaigns }
     let(:method) { :get }
     let(:params) do
       {
-        FieldNames: %w(Id Name),
-        SelectionCriteria: { Types: %w(TEXT_CAMPAIGN) }
+        field_names: [:id, :name],
+        selection_criteria: { types: %w(TEXT_CAMPAIGN) }
       }
     end
     let(:result) do
@@ -20,23 +19,19 @@ describe Direct::API::V5::Request do
       ]
     end
 
+    let(:request_params) do
+      Direct::API::V5::Request::ParamsBuilder.build(params)
+    end
     let(:request_body) do
-      {
-        method: method,
-        params: params
-      }
+      { method: method, params: request_params }
     end
     let(:response_body) do
-      {
-        result: {
-          Campaigns: result
-        }
-      }
+      { result: { Campaigns: result } }
     end
 
     before { stub_direct_api_request(service, request_body, response_body) }
 
-    subject { request.send(service, method, params) }
+    subject { described_class.send(client.settings, service, method, params) }
 
     it 'send request' do
       is_expected.to eq(result)

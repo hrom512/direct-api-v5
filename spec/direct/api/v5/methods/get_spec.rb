b2940339488a41200dd061f83cbccf1c838ac2da
hrom <roman@alltmb.ru>
2016-04-24 19:14:12 +0300

    API get method

--- spec/direct/api/v5/methods/get_spec.rb
+++ spec/direct/api/v5/methods/get_spec.rb
@@ -9,19 +9,35 @@ describe Direct::API::V5::Methods::Get do
 
   let(:service_class) { Direct::API::V5::Services::ServiceWithGetMethod }
   let(:service) { service_class.new(nil) }
+  let(:request_params) { service.request_params }
+
+  describe '#get' do
+    let(:result) { double }
+
+    it 'use call_method' do
+      allow(service).to receive(:call_method).with(:get, criteria: { param2: 'value2' }).and_return(result)
+      get_result = service.where(param1: 'value1').get(criteria: { param2: 'value2' })
+      expect(get_result).to eq(result)
+    end
+  end
 
   describe '#select' do
+    it 'return yourself' do
+      result = service.select(:field1, :field2)
+      expect(result).to eq(service)
+    end
+
     it 'add fields' do
-      params = service.select(:field1, :field2, :child1 => [:field3, :field4], :child2 => :field5).params
-      expect(params[:fields]).to eq([:field1, :field2])
-      expect(params[:child1_fields]).to eq([:field3, :field4])
-      expect(params[:child2_fields]).to eq([:field5])
+      service.select(:field1, :field2, :child1 => [:field3, :field4], :child2 => :field5)
+      expect(request_params[:fields]).to eq([:field1, :field2])
+      expect(request_params[:child1_fields]).to eq([:field3, :field4])
+      expect(request_params[:child2_fields]).to eq([:field5])
     end
 
     it 'update fields' do
-      params = service.select(:field1, child: [:field2, :field3]).select(:field4, :child => :field5).params
-      expect(params[:fields]).to eq([:field1, :field4])
-      expect(params[:child_fields]).to eq([:field2, :field3, :field5])
+      service.select(:field1, child: [:field2, :field3]).select(:field4, :child => :field5)
+      expect(request_params[:fields]).to eq([:field1, :field4])
+      expect(request_params[:child_fields]).to eq([:field2, :field3, :field5])
     end
   end
 
@@ -35,19 +51,24 @@ describe Direct::API::V5::Methods::Get do
       expect { service.limit(-1) }.to raise_error('Value must be more then 0')
     end
 
+    it 'return yourself' do
+      result = service.limit(10)
+      expect(result).to eq(service)
+    end
+
     it 'add limit' do
-      params = service.limit(10).params
-      expect(params[:page][:limit]).to eq(10)
+      service.limit(10)
+      expect(request_params[:page][:limit]).to eq(10)
     end
 
     it 'update limit' do
-      params = service.limit(10).limit(100).params
-      expect(params[:page][:limit]).to eq(100)
+      service.limit(10).limit(100)
+      expect(request_params[:page][:limit]).to eq(100)
     end
 
     it 'not overwrite offset' do
-      params = service.offset(100).limit(10).params
-      expect(params[:page][:offset]).to eq(100)
+      service.offset(100).limit(10)
+      expect(request_params[:page][:offset]).to eq(100)
     end
   end
 
@@ -61,19 +82,24 @@ describe Direct::API::V5::Methods::Get do
       expect { service.limit(-1) }.to raise_error('Value must be more then 0')
     end
 
+    it 'return yourself' do
+      result = service.offset(10)
+      expect(result).to eq(service)
+    end
+
     it 'add offset' do
-      params = service.offset(10).params
-      expect(params[:page][:offset]).to eq(10)
+      service.offset(10)
+      expect(request_params[:page][:offset]).to eq(10)
     end
 
     it 'update offset' do
-      params = service.offset(10).offset(100).params
-      expect(params[:page][:offset]).to eq(100)
+      service.offset(10).offset(100)
+      expect(request_params[:page][:offset]).to eq(100)
     end
 
     it 'not overwrite limit' do
-      params = service.limit(10).offset(100).params
-      expect(params[:page][:limit]).to eq(10)
+      service.limit(10).offset(100)
+      expect(request_params[:page][:limit]).to eq(10)
     end
   end
 end
